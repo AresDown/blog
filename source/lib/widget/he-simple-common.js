a836f108eb9431604b970e9a9548470964fe6859
WorldDawnAres <73381276+WorldDawnAres@users.noreply.github.com>
2026-01-21 14:11:38 +0800

    local he-simple

---
+++ source/lib/widget/he-simple-common.js
@@ -0,0 +1,10 @@
+(function (d) {
+  var c = d.createElement('link')
+  c.rel = 'stylesheet'
+  c.href = '/lib/widget/he-simple.css'
+  var s = d.createElement('script')
+  s.src = '/lib/widget/he-simple-ys.js'
+  var sn = d.getElementsByTagName('script')[0]
+  sn.parentNode.insertBefore(c, sn)
+  sn.parentNode.insertBefore(s, sn)
+})(document)
